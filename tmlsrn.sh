656fdfee2e04a1c98cbbbe7d892e2368d2f122db
karthhic <karthhic@users.noreply.github.com>
2017-10-02 14:53:50 +0300

    terminal screeen
    Just another security for working in Terminal with a rude reply

---
+++ tmlsrn.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+trap '' 2
+export IGNOREEOF=5
+clear
+echo "Welcome to Terminal"
+echo "==================="
+echo "Today "
+echo " "
+date
+echo " "
+cal
+echo "Enter the magic word : "
+read -s value
+if [ "yourpasswordhere" == $value ] ; then
+    cd ~/
+    clear
+    exit
+else
+  #!/bin/bash
+  clear
+ echo "        .-.        "
+ echo "        |U|        "
+ echo "        | |        "
+ echo "        | |        "
+ echo "       _| |_       "
+ echo "      | | | |-.    "
+ echo "     /|       |    "
+ echo "    | |       |    "
+ echo "    |         |    "
+ echo "    \         /    "
+ echo "     |       |     "
+ echo "     |       |     "
+ sleep 0.3
+ clear
+ echo " "
+ echo " "
+ echo "        .-.        "
+ echo "        | |        "
+ echo "       _| |_       "
+ echo "      | | | |-.    "
+ echo "     /|       |    "
+ echo "    | |       |    "
+ echo "    |         |    "
+ echo "    \         /    "
+ echo "     |       |     "
+ echo "     |       |     "
+ sleep 0.3
+ clear
+ echo " "
+ echo " "
+ echo " "
+ echo "        .-.        "
+ echo "       _| |_       "
+ echo "      | | | |-.    "
+ echo "     /|       |    "
+ echo "    | |       |    "
+ echo "    |         |    "
+ echo "    \         /    "
+ echo "     |       |     "
+ echo "     |       |     "
+ sleep 0.3
+ clear
+ echo " "
+ echo " "
+ echo " "
+ echo " "
+ echo "       _.-._        "
+ echo "      | | | |-.    "
+ echo "     /|       |    "
+ echo "    | |       |    "
+ echo "    |         |    "
+ echo "    \         /    "
+ echo "     |       |     "
+ echo "     |       |     "
+ sleep 0.3
+ clear
+ echo " "
+ echo " "
+ echo " "
+ echo "        .-.        "
+ echo "       _| |_       "
+ echo "      | | | |-.    "
+ echo "     /|       |    "
+ echo "    | |       |    "
+ echo "    |         |    "
+ echo "    \         /    "
+ echo "     |       |     "
+ echo "     |       |     "
+ sleep 0.3
+ clear
+ echo " "
+ echo " "
+ echo "        .-.        "
+ echo "        | |        "
+ echo "       _| |_       "
+ echo "      | | | |-.    "
+ echo "     /|       |    "
+ echo "    | |       |    "
+ echo "    |         |    "
+ echo "    \         /    "
+ echo "     |       |     "
+ echo "     |       |     "
+ sleep 0.3
+ clear
+ echo "        .-.        "
+ echo "        |U|        "
+ echo "        | |        "
+ echo "        | |        "
+ echo "       _| |_       "
+ echo "      | | | |-.    "
+ echo "     /|       |    "
+ echo "    | |       |    "
+ echo "    |         |    "
+ echo "    \         /    "
+ echo "     |       |     "
+ echo "     |       |     "
+ sleep 0.3
+ $0
+ exit
+fi
+trap 2
